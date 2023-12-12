873422745589556dddd574b25845fa012c59d65a
Zelong Li <87303807+liz435@users.noreply.github.com>
2023-12-12 00:00:33 -0500

    working

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from io import BytesIO
 
 import requests
 import cv2
-import os
 from PIL import Image
 import io
 import time
@@ -11,6 +10,8 @@ import time
 app = Flask(__name__)
 
 API_URL = "https://api-inference.huggingface.co/models/Salesforce/blip-image-captioning-large"
+API_URL2 = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+
 headers = {"Authorization": "Bearer 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"}
 
 # def serve_pil_image(pil_img):
@@ -20,7 +21,8 @@ headers = {"Authorization": "Bearer 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"}
 #     return send_file(img_io, mimetype='image/jpeg')
 
 def text_to_image(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
+    response = requests.post(API_URL2, headers=headers, json=payload)
+    print(type(response.content))
     return response.content
 
 
@@ -49,23 +51,23 @@ def home():
     filename = None
     caption = None
     image = None
-    init = False
+    image_buffer = None
+    image_name = None
 
     
     if request.method == "POST":
         filename = capture_image()
         caption = describtion(filename)
-        init = True
 
         if caption:
-
             image_bytes = text_to_image({
-                "inputs" : "cat sitting on a rock",
+                "inputs" : caption[0]['generated_text'],
             })
             try:
+                print(image_bytes)
                 image_buffer = io.BytesIO(image_bytes)
                 image = Image.open(image_buffer)
-                image.save("output.jpg", format="JPEG")
+                image.save("static/output.jpg", format="JPEG")
             except Exception as e:
                 print(f"Unexpected error: {e}")
                 
